1c3f98e8c413effc232d01261222a26ac2e4b97d
Ayush0054 <ayush5aprill@gmail.com>
2023-05-22 20:16:49 +0530

    added video

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,18 @@ import WeatherApp from "./page/weather";
 function App() {
   return (
     <div className="app">
+      <video
+        id="background-video"
+        controls
+        loop
+        muted
+        poster="https://assets.codepen.io/6093409/river.jpg"
+      >
+        <source
+          src="https://assets.codepen.io/6093409/river.mp4"
+          type="video/mp4"
+        />
+      </video>
       <Header />
       <WeatherApp />
     </div>
