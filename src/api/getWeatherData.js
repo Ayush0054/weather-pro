342d5558044aa627819670a515356c7711918456
Ayush0054 <ayush5aprill@gmail.com>
2023-05-22 20:01:26 +0530

    api

---
+++ src/api/getWeatherData.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_KEY = "";
+
+const getWeatherData = async (city) => {
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    );
+    return {
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      weather: response.data.weather[0].description,
+      date: new Date().toLocaleDateString(),
+    };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export default getWeatherData;
